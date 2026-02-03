0be482945ec7821b775b52719ea83b7af40ff36c
VidhiSoni4 <Vidhi-S24@users.noreply.github.com>
2026-02-04 00:41:57 +0530

    change in asset.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,6 +4,8 @@ import add_icon_white from './add_icon_white.png'
 import order_icon from './order_icon.svg'
 import upload_area from './upload_area.svg'
 
+export const url="http://localhost:4000";
+
 export const assets={
     logo,
     profile,
